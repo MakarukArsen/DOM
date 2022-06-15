5b73d271bc7ee23cc43ba44f806c7d79570eab69
MakarukArsen <makarukarsen@gmail.com>
2022-06-15 16:36:14 +0300

    1.0

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log("123")
+// Маніпулювання елементів створених в html - РОЗКОМЕНТУЙТЕ html!
+// const container = document.body.querySelector(".container");
+// const squareCollection = document.body.querySelectorAll(".square");
+// function getRandomColor() {
+//     return Math.floor(Math.random() * (256 - 1) + 1);
+// }
+// function generateBlocksInterval() {
+//     setTimeout(()=> {
+//         container.style.display = "flex";
+//         setInterval(() => {
+//             for(item of squareCollection){
+//                 item.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}`;
+//             }
+//         }, 1000);
+//     }, 3000);
+// }
+// generateBlocksInterval();
+
+// Маніпулювання елементів створених в JavaScript - ЗАКОМЕНТУЙТЕ html :)
+const body = document.body;
+body.insertAdjacentHTML("afterbegin", `<div class="container"></div>`);
+body.insertAdjacentHTML("afterbegin", `<h1>HW#11 DOM</h1>`);
+const container = body.querySelector(".container");
+for(let i = 1; i <= 25; i++) {
+    container.insertAdjacentHTML("afterbegin", `<div class="square"></div>`);
+}
+const squareCollection = container.querySelectorAll(".square");
+
+function getRandomColor() {
+    return Math.floor(Math.random() * (256 - 1) + 1);
+}
+function generateBlocksInterval() {
+    setTimeout(()=> {
+        container.style.display = "flex";
+        setInterval(() => {
+            for(item of squareCollection){
+                item.style.backgroundColor = `rgb(${getRandomColor()}, ${getRandomColor()}, ${getRandomColor()}`;
+            }
+        }, 1000);
+    }, 3000);
+}
+generateBlocksInterval();
